15c95264e75a0b214e3914ec51a7645041f980a3
Johan Attali <johan@smartnsoft.com>
2012-04-16 09:48:16 +0200

    added NSDate Category

---
+++ SnSFramework/SnSFramework/ios4me/Classes/Extensions/NSDate+SnSExtension.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+SnSExtension.h
+//  ios4me
+//
+//  Created by Johan Attali on 13/04/12.
+//  Copyright (c) 2012 Smart&Soft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (SnSExtension)
+
+@end
---
+++ SnSFramework/SnSFramework/ios4me/Classes/Extensions/NSDate+SnSExtension.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+SnSExtension.m
+//  ios4me
+//
+//  Created by Johan Attali on 13/04/12.
+//  Copyright (c) 2012 Smart&Soft. All rights reserved.
+//
+
+#import "NSDate+SnSExtension.h"
+
+@implementation NSDate (SnSExtension)
+
+@end
