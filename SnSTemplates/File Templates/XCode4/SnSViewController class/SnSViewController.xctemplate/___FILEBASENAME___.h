95071da8dba6db8a180512fa486a3f176dbc0c04
Johan Attali <johan@smartnsoft.com>
2012-04-23 15:57:41 +0200

    removed all sim links replaced by hard copy

--- SnSTemplates/File Templates/XCode4/SnSViewController class/SnSViewController.xctemplate/___FILEBASENAME___.h
+++ SnSTemplates/File Templates/XCode4/SnSViewController class/SnSViewController.xctemplate/___FILEBASENAME___.h
@@ -1,1 +1,26 @@
-../../../XCode3/SnSViewController class/SnSViewController subclass.pbfiletemplate/class.h
+/* 
+ * Copyright (C) 2009-2010 Smart&Soft.
+ * All rights reserved.
+ *
+ * The code hereby is the full property of Smart&Soft, SIREN 517 961 744.
+ * http://www.smartnsoft.com - contact@smartnsoft.com
+ * 
+ * You are not allowed to use the source code or the resulting binary code, nor to modify the source code, without prior permission of the owner.
+ */
+
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface «FILEBASENAMEASIDENTIFIER» : SnSViewController 
+{
+
+}
+
+@end
