b06e4b4306eb5b7ebb2228b3d0da7f656f9ad895
Johan Attali <johan@smartnsoft.com>
2012-05-28 17:41:13 +0200

    added NSArray extensions

---
+++ SnSFramework/SnSFramework/ios4me/Classes/Extensions/NSArray+SnSExtension.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+SnSExtension.h
+//  ios4me
+//
+//  Created by Johan Attali on 28/05/12.
+//  Copyright (c) 2012 Smart&Soft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (SnSExtension)
+
+- (id)firstObject;
+
+@end
