94ab581c18cdc77eb109ca2d6074255278c182d5
apouche <johan@smartnsoft.com>
2012-11-30 18:03:24 +0100

    adding tag-project.sh script

---
+++ SnSScripts/tag-project.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# no need to go further is configuration doesn't match AppStore
+if [[ `echo ${CONFIGURATION}|grep -iE "(Release|Ad\s*Hoc)"` == "" ]]; then
+  echo "Skippng Tagging for Configuration [${CONFIGURATION}]"
+  exit 0
+fi
+
+
+
+I4M_PRJ_VERSION=`grep "CFBundleVersion" $INFOPLIST_FILE -A 1 | grep -oE "\w+\.\w+"`
+I4M_PRJ_TAG=`echo "${PROJECT_NAME}_${I4M_PRJ_VERSION}" | sed "s/\./-/g" |sed "s/\./-/g"`
+
+echo "Creating Tag : $I4M_PRJ_TAG for  $PROJECT_NAME"
+
+cd ${PROJECT_DIR}
+
+if [[ `git tag -l ${I4M_PRJ_TAG}` != "" ]]; then
+  echo "Error - Tag ${I4M_PRJ_TAG} is already existing"
+  exit 1
+else
+  git tag ${I4M_PRJ_TAG}
+  git push --tags
+fi
+
